82f8bf35551c44deb00a0975ed75bffbc2038375
Guilherme Jesuino Ramires <guilhermejramires@gmail.com>
2022-09-25 12:25:26 -0300

    Create scenes

---
+++ projects/third_person_platform_character/scripts/third_person_platform_character.gd
@@ -0,0 +1,31 @@
+class_name ThirdPersonCharacter
+extends CharacterBody3D
+
+
+@export var movement_speed: float = 5.0
+@export var jump_impulse: float = 4.5
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_impulse
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * movement_speed
+		velocity.z = direction.z * movement_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, movement_speed)
+		velocity.z = move_toward(velocity.z, 0, movement_speed)
+
+	move_and_slide()
