763d3b749fc2ca162d25743a9f3fbd215d0cbf4a
Guilherme Jesuino Ramires <guilhermejramires@gmail.com>
2022-09-25 10:44:43 -0300

    Add bg music

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -2,14 +2,19 @@ extends Control
 
 
 @onready var animation: AnimationPlayer = $animation_player
+@onready var bg_music:AudioStreamPlayer = $back_music
 
-var can_acess: bool = false
+var can_play_music: bool = false
 
 
 func _ready():
 	animation.play("start")
 
+func _process(_delta):
+	if can_play_music and not bg_music.playing:
+		bg_music.play()
+
 
 func _on_animation_player_animation_finished(_anim_name):
-	can_acess = true
+	can_play_music = true
 
